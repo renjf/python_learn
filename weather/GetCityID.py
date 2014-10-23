10886068b6fd3eae0c9b7b2bc64079f7c9b8deec
renjf <renjunfengly2007@126.com>
2014-10-23 08:38:17 +0800

    141023  09:38

--- weather/GetCityID.py
+++ weather/GetCityID.py
@@ -8,38 +8,54 @@ import HTMLParser
 import httplib  
 from bs4 import BeautifulSoup  
 
-def GetProvinceURL(province):  
-	response = urllib2.urlopen('http://www.weather.com.cn/textFC/hn.shtml')  
-	htmlByte = response.read()  
-	htmlStr = htmlByte.decode("utf8")  
-	soup2 = BeautifulSoup(htmlStr)  
-	div = soup2.find("div", class_ = "lqcontentBoxheader")  
-	lista = div.find_all("a")  
-	provinceURL = "http://www.weather.com.cn"  
-	for aItem in lista:  
-		if aItem.text == province:  
-			provinceURL = provinceURL + aItem["href"]  
-			break  
+def GetProvinceURL(province): 
+	try: 
+		response = urllib2.urlopen('http://www.weather.com.cn/textFC/hn.shtml')  
+		htmlByte = response.read()  
+		htmlStr = htmlByte.decode("utf8")  
+		soup2 = BeautifulSoup(htmlStr)  
+		div = soup2.find("div", class_ = "lqcontentBoxheader")  
+		lista = div.find_all("a")  
+		provinceURL = "http://www.weather.com.cn"  
+		for aItem in lista:  
+			if aItem.text == province:  
+				provinceURL = provinceURL + aItem["href"]  
+				break  
+		return provinceURL  
+	except:
+		print 'urlopen url:http://www.weather.com.cn/textFC/hn.shtml error'
+		return ''
 
-	return provinceURL  
-
-def GetCityURL(provinceURL, city):  
-	response = urllib2.urlopen(provinceURL)  
+def GetCityURL(provinceURL, city): 
+	if provinceURL=='' or city=='':
+		return ''
+	try:
+		response = urllib2.urlopen(provinceURL)  
+	except:
+		print 'urlopen url:%s error' %(provinceURL)
+		return ''
 	htmlByte = response.read()  
 	htmlStr = htmlByte.decode("utf8")  
 
 	soup2 = BeautifulSoup(htmlStr)  
 	div = soup2.find("div", class_ = "hanml")  
 	lista = div.find_all("a", text = city)  
-	cityURL = lista[0]["href"].replace("www.weather.com.cn/weather", "m.weather.com.cn/data")  
+	cityURL = lista[0]["href"].replace("www.weather.com.cn/weather", "m.weather.com.cn/atad")   #new interface
 	cityURL = cityURL.replace("shtml", "html")  
 	return cityURL  
 	''''' 
 # GetProvinceURL 测试代码 
 	print GetProvinceURL(u"广东") 
-	'''  
+
+'''	
+
+'''
 # GetProvinceURL 测试代码  
 provinceURL = GetProvinceURL(u"广东")  
 print provinceURL  
 cityURL = GetCityURL(provinceURL, u"广州")  
 print cityURL  
+
+'''
+
+
--- weather/main.py
+++ weather/main.py
@@ -6,24 +6,51 @@ import GetCity
 import GetCityID  
 import GetCityWeather  
 
+def print_weather_info(st):
+	try:
+		ss = st["weatherinfo"] 
+		print '城市：%s 日期：%s %s 温度：%s 天气：%s' %(ss["city"],ss["date_y"],ss["week"],ss["temp1"],ss["weather1"]  )
+	except:
+		print 'can not get weather information'
+		
 
-ip = GetIP.GetIP()  
-print ip  
+def auto_get_weather_info():
+	ip = GetIP.GetIP()  
+	print ip  
 
-# 国家/省份/城市  
-city = ["", "", ""]  
-GetCity.GetCity(ip, city)  
-print city[0], city[1], city[2]  
+	# 国家/省份/城市  
+	city = ["", "", ""]  
+	GetCity.GetCity(ip, city)  
+	print city[0], city[1], city[2]  
 
-provinceURL = GetCityID.GetProvinceURL(city[1])  
-cityURL = GetCityID.GetCityURL(provinceURL, city[2])  
-print provinceURL  
-print cityURL  
+	provinceURL = GetCityID.GetProvinceURL(city[1])  
+	cityURL = GetCityID.GetCityURL(provinceURL, city[2])  
+	print provinceURL  
+	print cityURL  
 
-st = GetCityWeather.GetCityWeather(cityURL)  
-ss = st["weatherinfo"]  
-print ss["city"]  
-print ss["date_y"]  
-print ss["week"]  
-print ss["temp1"]  
-print ss["weather1"]  
+	st = GetCityWeather.GetCityWeather(cityURL)  
+	#st = GetCityWeather.SwitchGetCityWeather(cityURL) 
+	print_weather_info(st) 
+
+def  weatherinfo(city1,city2):
+	provinceURL = GetCityID.GetProvinceURL(city1)  
+	cityURL = GetCityID.GetCityURL(provinceURL, city2)  
+	print provinceURL  
+	print cityURL  
+
+	st = GetCityWeather.GetCityWeather(cityURL)  
+	print_weather_info(st) 
+
+''''
+fp_r=open('./config/province_city_id.txt',"r");
+while True:
+	line=fp_r.readline().decode("utf-8")
+	if line == '':
+		break;
+	if line[0] =='\n' or line[0] =='#':
+		continue
+	city_map=line.replace('\n','').replace('\r','').split('\t')
+	print '%s %s' %(city_map[0],city_map[1])
+	weatherinfo(city_map[0],city_map[1])
+'''
+auto_get_weather_info()
